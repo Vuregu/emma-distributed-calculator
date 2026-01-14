3d5b34b6e0e6a674b2d11c96f8c9b87e3d629c31
Vreg Andreasyan <vreg.andreasyan@gmail.com>
2026-01-14 03:00:52 +0100

    fix: optional tls when using redis

--- apps/web/lib/queue.ts
+++ apps/web/lib/queue.ts
@@ -12,12 +12,14 @@ export const getCalculationQueue = () => {
     const REDIS_PORT = parseInt(process.env.REDIS_PORT || '6379');
     const REDIS_USERNAME = process.env.REDIS_USERNAME || undefined;
     const REDIS_PASSWORD = process.env.REDIS_PASSWORD || undefined;
+    const REDIS_TLS = process.env.REDIS_TLS || undefined;
 
     const connection = new Redis({
         host: REDIS_HOST,
         port: REDIS_PORT,
         username: REDIS_USERNAME,
         password: REDIS_PASSWORD,
+        tls: REDIS_TLS ? {} : undefined,
         maxRetriesPerRequest: null,
     });
 
