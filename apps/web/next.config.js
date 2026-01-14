a737826cb3da121b78f6ae52ec65356c95ad26c2
Vreg Andreasyan <vreg.andreasyan@gmail.com>
2026-01-14 19:11:00 +0100

    fix: prevent cdn cache control

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
+    async headers() {
+        return [
+            {
+                source: '/:path*',
+                headers: [
+                    {
+                        key: 'CDN-Cache-Control',
+                        value: 'no-store',
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 module.exports = nextConfig;
